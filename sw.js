0bf981599c8b7b183f328c5913d442b43ce1d272
Haruki <uwho2@naver.com>
2026-03-27 21:29:21 +0900

    HARUKI 웹앱

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE = 'haruki-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html','./icon.png']))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
